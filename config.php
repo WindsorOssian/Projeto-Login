d9bed2aa857605b6ad7c4b5120a8894aff54c7dc
WindsorOssian <windsorooo@hotmail.com>
2025-11-13 12:22:02 -0300

    config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+// Configurações básicas
+date_default_timezone_set('America/Sao_Paulo');
+
+define('APP_NAME', 'Sistema de Login PHP');
+define('VERSION', '1.0');
+
+// Usuário e senha simulados (em produção viriam do banco)
+$usuarioPadrao = [
+    'email' => 'teste@exemplo.com',
+    'senha' => '1234'
+];
